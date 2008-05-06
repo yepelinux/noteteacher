db8d22be5b9fbe7e5107fbfa75a58a0fd6955fab
zenebord <zenebord@386eef43-734c-0410-8c00-9576afc8d0bd>
2008-05-06 19:15:47 +0000

    config files

---
+++ trunk/noteTeacher/model/Note.java
@@ -0,0 +1,28 @@
+package model;
+
+public class Note {
+	
+	private String name;
+	private double frequence;
+	
+	public Note(){
+	}	
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public double getFrequence() {
+		return frequence;
+	}
+	public void setFrequence(double frequence) {
+		this.frequence = frequence;
+	}
+	
+	
+
+}
