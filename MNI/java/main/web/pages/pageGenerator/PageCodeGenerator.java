d9afe9b68cf220facba5902302fd6a790bfdb3e2
ra.cardozo9 <ra.cardozo9@386eef43-734c-0410-8c00-9576afc8d0bd>
2008-06-19 21:19:45 +0000

    nothing to say

--- MNI/java/main/web/pages/pageGenerator/PageCodeGenerator.java
+++ MNI/java/main/web/pages/pageGenerator/PageCodeGenerator.java
@@ -2,31 +2,22 @@ package pages.pageGenerator;
 
 import generadoresCodigo.GeneradorCodigoPagina;
 
-import java.io.BufferedWriter;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStreamWriter;
 import java.util.HashMap;
 import java.util.List;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipOutputStream;
 
 import pages.basePage.BasePage;
-
-import wicket.IRequestTarget;
-import wicket.RequestCycle;
 import wicket.ajax.AjaxRequestTarget;
 import wicket.ajax.markup.html.form.AjaxSubmitLink;
 import wicket.markup.html.WebMarkupContainer;
 import wicket.markup.html.form.Form;
 import wicket.markup.html.form.TextArea;
 import wicket.markup.html.form.TextField;
+import wicket.markup.html.link.Link;
 import wicket.model.PropertyModel;
-import wicket.model.StringResourceModel;
 import wicket.protocol.http.WebResponse;
-import wicket.util.io.Streams;
 
 
 @SuppressWarnings("serial")
@@ -79,77 +70,56 @@ public class PageCodeGenerator extends BasePage {
 			};
 			add(generateButton);
 			
-			AjaxSubmitLink downloadButton = new AjaxSubmitLink("downloadButton", InputForm.this){
-
-				@Override
-				protected void onSubmit(AjaxRequestTarget target, Form form) {
-					InputForm.this.onSubmit();
-				}
-			};
-			add(downloadButton);
-
+			
+			
+			Link downloadLink = new Link("downloadButton"){ 
+				private static final long serialVersionUID = 1L;
+				 
+				public void onClick(){ 
+					
+					if(getHtmlText().length()> 0){
+						String tableCode = getHtmlText(); 
+						BufferedInputStream bis = null; 
+						BufferedOutputStream bos = null; 
+						try { 
+							String ContentType = "text/html"; 
+							getResponse().setContentType(ContentType); 
+							((WebResponse)getResponse()).setAttachmentHeader("page.html"); 
+							ByteArrayInputStream bais = new ByteArrayInputStream(tableCode.getBytes()); 
+							bis = new BufferedInputStream(bais); 
+							bos=new BufferedOutputStream(getResponse().getOutputStream()); 
+							byte[] buff=new byte[2048]; 
+							int bytesread; 
+							while((bytesread=bis.read(buff,0,buff.length)) != -1) { 
+								bos.write(buff,0,bytesread); 
+							} 
+							bos.flush(); 
+						} catch (IllegalStateException e) {
+							//Do nothing 
+						} catch (Exception e) {
+							//Do nothing 
+						} finally{ 
+						 	try { 
+						 		bos.close();
+						 		bos=null; 
+						 		bis.close();
+						 		bis=null; 
+						 
+						} catch (IllegalStateException e) {
+							//Do nothing
+						} catch (Exception e) {
+							//Do nothing
+						}
+					}
+				}		 
+			}};
+			add(downloadLink);
 		}
 		
 		@Override
 		protected void onSubmit() {
-			System.err.println("down");
-			downloadHtml(getHtmlText());
-//			downloadSendingInZip();
 		}
 	}
-
-	private void downloadHtml(final String sending) {
-		
-		if (sending.length() > 0){
-			RequestCycle.get().setRequestTarget(new IRequestTarget() {
-			
-				public void detach(RequestCycle requestCycle) {}
-	
-				public Object getLock(RequestCycle requestCycle) { return null; }
-				
-				public void respond(RequestCycle requestCycle){
-					WebResponse response = (WebResponse)requestCycle.getResponse();
-					
-					// Edison specs determines that chars 6 to 19 are the reference field
-					String reference = sending.substring(6, 19);
-					int spacePosition = sending.substring(6, 19).indexOf(' ');
-					
-					response.setAttachmentHeader(reference.substring(0, spacePosition) + ".txt");
-					
-					try {
-						
-						ByteArrayOutputStream ostream = new ByteArrayOutputStream();
-						BufferedWriter out = new BufferedWriter(new OutputStreamWriter(ostream, "Cp850"));
-						out.write(sending);
-						out.close();
-
-						InputStream istream = new ByteArrayInputStream(ostream.toByteArray()); 
-						
-						try	{
-							
-							Streams.copy(istream, response.getOutputStream());
-							// Exception due to checked exception							
-						} catch (Exception e) {
-							throw new RuntimeException(e);
-							
-						} finally {
-							
-							try	{
-								istream.close();
-							} catch (Exception e) {
-								throw new RuntimeException(e);
-							}
-						}
-						
-					} catch (Exception e) {
-						// Exception due to checked exception
-						throw new RuntimeException(e);
-					}
-				}
-			});
-		}
-	}
-
 	
 	public GeneradorCodigoPagina getGenerador() {return generador;	}
 
@@ -166,94 +136,4 @@ public class PageCodeGenerator extends BasePage {
 	public List<HashMap<String, Object>> getPhotos() {return photos;}
 
 	public void setPhotos(List<HashMap<String, Object>> photos) {this.photos = photos;}
-	
-	private void downloadSendingInZip(){
-		
-		RequestCycle.get().setRequestTarget(new IRequestTarget() {
-			
-			public void detach(RequestCycle requestCycle) {}
-
-			public Object getLock(RequestCycle requestCycle) { return null; }
-			
-			public void respond(RequestCycle requestCycle){
-				
-				WebResponse response = (WebResponse)requestCycle.getResponse();
-				
-				// Zip file
-				ByteArrayOutputStream zipout = new ByteArrayOutputStream();				
-		        ZipOutputStream s = new ZipOutputStream(zipout);
-		        
-		        s.setLevel(6);
-				
-				try{
-											
-						String sending = getHtmlText();
-						
-						byte[] buf = new byte[sending.length()];
-						
-						String reference = sending.substring(6, 19);
-						int spacePosition = sending.substring(6, 19).indexOf(' ');
-						
-						// Create stream file with the sending
-						ByteArrayOutputStream ostream = new ByteArrayOutputStream();
-						BufferedWriter out = new BufferedWriter(new OutputStreamWriter(ostream, "Cp850"));
-						out.write(sending);
-						out.close();
-	
-						InputStream istream = new ByteArrayInputStream(ostream.toByteArray());
-											
-						// Add new entry in zip file
-						ZipEntry entry = new ZipEntry(reference.substring(0, spacePosition));
-												
-						s.putNextEntry(entry);
-						
-						// Write the sending in the entry					
-						istream.read(buf,0,buf.length);		            
-			            s.write(buf,0,buf.length);
-			            
-						s.closeEntry();					
-					
-					s.finish();
-		            s.close();
-		            System.err.println("end down");
-	            
-				}catch(IOException e){
-					throw new RuntimeException(e);	
-				}
-												
-		            
-				String zipFileName = new StringResourceModel("zipFileName", PageCodeGenerator.this, null).getString();
-		        
-				response.setAttachmentHeader(zipFileName + ".zip");
-				
-				try {
-						
-					InputStream istream = new ByteArrayInputStream(zipout.toByteArray());
-					
-					try	{						
-						
-						Streams.copy(istream, response.getOutputStream());
-						
-						// Exception due to checked exception							
-					} catch (Exception e) {
-						
-						throw new RuntimeException(e);
-						
-					} finally {
-						
-						try	{
-							istream.close();
-						} catch (Exception e) {
-							throw new RuntimeException(e);
-						}
-					}
-					
-				} catch (Exception e) {
-					// Exception due to checked exception
-					throw new RuntimeException(e);
-				}
-			}
-		});
-	}
-
 }
